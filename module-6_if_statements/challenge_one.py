639e9c1bd97fc0b346d82215ac95530f154da625
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-06-25 10:41:07 +0100

    doing some more challenges (module-6: if statements)

---
+++ module-6_if_statements/challenge_one.py
@@ -0,0 +1,7 @@
+month = input("Month: ").upper()
+
+if month == "SEP" or month == "APR" \
+        or month == "JUN" or month == "NOV":
+    print("There are 30 days in this month")
+else:
+    print("Sorry, We dont have data")
---
+++ module-6_if_statements/challenge_two.py
@@ -0,0 +1,16 @@
+favMovie = input("Enter your favorite movie : ").lower()
+favBook = input("Enter your favorite book : ").lower()
+favEvent = input("Enter your favorite event : ").lower()
+
+if favMovie == "star wars" \
+        and favBook == "lord of the rings" \
+        and favEvent == "comicon":
+    print("You and I should hang out")
+else:
+    print("We are not the same, please try again")
+
+# _country = "VIETNAM"
+# pet = "BEAVER"
+#
+# if _country == "CANADA" and pet == "MOOSE" or pet == "BEAVER":
+#     print("Do you play hockey too ?")
