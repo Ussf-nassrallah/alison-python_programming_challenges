fdec6528066bd41cd23dbad6a1c8c6c2292bed4d
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-02 11:39:57 +0100

    m9: 0-add_integer completed

--- module-9_testing/challenges/0-add_integer.py
+++ module-9_testing/challenges/0-add_integer.py
@@ -5,4 +5,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    return int(a + b)
+    return int(a) + int(b)
